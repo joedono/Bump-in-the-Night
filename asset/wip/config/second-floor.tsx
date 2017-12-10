cf0b90373d335fc52ed2ad7f582c48f8dbc90858
joedono <zidane320@yahoo.com>
2017-12-09 22:10:34 -0500

    Converted all floors except basement

--- asset/wip/config/second-floor.tsx
+++ asset/wip/config/second-floor.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset name="second-floor" tilewidth="16" tileheight="16" tilecount="4" columns="2">
+<tileset name="Second Floor" tilewidth="16" tileheight="16" tilecount="4" columns="2">
  <image source="../../image/floor3.png" width="32" height="32"/>
 </tileset>
