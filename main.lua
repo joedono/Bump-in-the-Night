daef9c24c4e32b32e4d6a194470bc776c669ea73
joedono <zidane320@yahoo.com>
2017-12-09 20:54:34 -0500

    Restarted game state

--- main.lua
+++ main.lua
@@ -1,16 +1,15 @@
 GameState = require "lib/hump/gamestate";
 Class = require "lib/hump/class";
-Vector = require "lib/hump/vector-light";
+Vector = require "lib/hump/vector";
+Bump = require "lib/bump";
 require "lib/general";
 
 require "config/constants";
 
-require "state/state_title";
-require "state/state_game";
+require "state/state-game";
 
 function love.load()
 	GameState.registerEvents();
-	-- GameState.switch(State_Title);
 	GameState.switch(State_Game);
 end
 
---
+++ state/state-game.lua
@@ -0,0 +1,27 @@
+require "player";
+
+State_Game = {};
+
+function State_Game:init()
+	BumpWorld = Bump.newWorld(50);
+end
+
+function State_Game:enter()
+
+end
+
+function State_Game:keypressed(key, unicode)
+
+end
+
+function State_Game:keyreleased(key, unicode)
+
+end
+
+function State_Game:update(dt)
+
+end
+
+function State_Game:draw()
+	
+end
