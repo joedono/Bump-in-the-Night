8f0cd9734a81a6d30fb1b7697a82be456e8aa517
joedono <zidane320@yahoo.com>
2017-12-11 22:47:15 -0500

    Added fullscreen mode

--- main.lua
+++ main.lua
@@ -10,6 +10,28 @@ require "config/constants";
 require "state/state-game";
 
 function love.load()
+	love.window.setFullscreen(FULLSCREEN);
+
+  CANVAS = love.graphics.newCanvas(SCREEN_WIDTH, SCREEN_HEIGHT);
+
+  local w = love.graphics.getWidth();
+  local h = love.graphics.getHeight();
+
+  local scaleX = 1;
+  local scaleY = 1;
+
+  if FULLSCREEN then
+    scaleX = w / SCREEN_WIDTH;
+    scaleY = h / SCREEN_HEIGHT;
+  end
+
+  CANVAS_SCALE = math.min(scaleX, scaleY);
+
+  CANVAS_OFFSET_X = w / 2 - (SCREEN_WIDTH * CANVAS_SCALE) / 2;
+  CANVAS_OFFSET_Y = h / 2 - (SCREEN_HEIGHT * CANVAS_SCALE) / 2;
+
+	love.graphics.setDefaultFilter("nearest", "nearest");
+
 	GameState.registerEvents();
 	GameState.switch(State_Game);
 end
--- state/state-game.lua
+++ state/state-game.lua
@@ -83,5 +83,12 @@ function State_Game:update(dt)
 end
 
 function State_Game:draw()
-	self.player:draw();
+	CANVAS:renderTo(function()
+		love.graphics.clear();
+		
+    self.player:draw();
+  end);
+
+  love.graphics.setColor(255, 255, 255);
+  love.graphics.draw(CANVAS, CANVAS_OFFSET_X, CANVAS_OFFSET_Y, 0, CANVAS_SCALE, CANVAS_SCALE);
 end
