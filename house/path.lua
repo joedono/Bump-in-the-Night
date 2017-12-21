0d3b261fe0bb5618b17109588f62542fd9e1ad10
joedono <zidane320@yahoo.com>
2017-12-21 17:40:44 -0500

    Got rid of source origin

--- house/path.lua
+++ house/path.lua
@@ -2,28 +2,18 @@ Path = Class {
   init = function (self, floor, source)
     self.source = source;
     self.origin = {
-      x = source.x,
-      y = source.y,
+      x = source.x + floor.origin.x,
+      y = source.y + floor.origin.y,
       w = source.width,
       h = source.height
     };
 
     self.center = {
-      x = source.x + source.width / 2,
-      y = source.y + source.height / 2
+      x = self.origin.x + self.origin.w / 2,
+      y = self.origin.y + self.origin.h / 2
     };
 
-    self.offset = {
-      x = self.origin.x + floor.origin.x,
-      y = self.origin.y + floor.origin.y
-    };
-
-    self.offsetCenter = {
-      x = self.center.x + floor.origin.x,
-      y = self.center.y + floor.origin.y
-    };
-
-    BumpWorld:add(self, self.offset.x, self.offset.y, self.origin.w, self.origin.h);
+    BumpWorld:add(self, self.origin.x, self.origin.y, self.origin.w, self.origin.h);
     self.connections = {};
     self.type = "path";
   end
@@ -40,13 +30,13 @@ end
 
 function Path:draw()
   love.graphics.setColor(100, 255, 0);
-  love.graphics.rectangle("fill", self.offset.x, self.offset.y, self.origin.w, self.origin.h);
+  love.graphics.rectangle("fill", self.origin.x, self.origin.y, self.origin.w, self.origin.h);
 
   love.graphics.setColor(255, 255, 255);
   for index, connection in pairs(self.connections) do
     love.graphics.line(
-      self.offset.x, self.offset.y,
-      connection.path.offset.x + connection.path.origin.w, connection.path.offset.y + connection.path.origin.h
+      self.origin.x, self.origin.y,
+      connection.path.origin.x + connection.path.origin.w, connection.path.origin.y + connection.path.origin.h
     );
   end
 end
