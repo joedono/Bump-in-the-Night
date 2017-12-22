1c3d82fff15a72d2716514a1d77109cee077754f
joedono <zidane320@yahoo.com>
2017-12-22 16:25:56 -0500

    Started work on main-floor tiles

--- asset/wip/config/main-floor.tsx
+++ asset/wip/config/main-floor.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset name="Main Floor" tilewidth="32" tileheight="32" tilecount="1" columns="1">
- <image source="../../image/floor2.png" width="32" height="32"/>
+<tileset name="Main Floor" tilewidth="32" tileheight="32" tilecount="117" columns="9">
+ <image source="../../image/tiles/tilesetHouse.png" trans="ffffff" width="288" height="416"/>
 </tileset>
