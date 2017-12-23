a13d12c115872df8e4dac4792276361cfaf4c02d
joedono <zidane320@yahoo.com>
2017-12-22 22:49:56 -0500

    Reworked *many* of the tiles

--- asset/wip/config/attic.tsx
+++ asset/wip/config/attic.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset name="Attic" tilewidth="32" tileheight="32" tilecount="1" columns="1">
- <image source="../../image/floor1.png" width="32" height="32"/>
+<tileset name="Attic" tilewidth="32" tileheight="32" tilecount="117" columns="9">
+ <image source="../../image/tiles/tilesetHouse.png" width="288" height="416"/>
 </tileset>
