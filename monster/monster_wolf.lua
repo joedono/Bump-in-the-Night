b966d4fce424c940626a910e2d4cf6d7395242b0
joedono <zidane320@yahoo.com>
2017-12-25 10:19:54 -0500

    Added behavior notes to wolf and bear

--- monster/monster_wolf.lua
+++ monster/monster_wolf.lua
@@ -1,13 +1,26 @@
+--[[
+States:
+Idle - Hasn't seen or heard player and player hasn't dropped meat. Randomly walk around sniffing and eating things. Avoid traps
+Alert - Has heard the player. Look towards sound. If can't see player for some time, walk towards source of sound. If still can't see player for some time, go back to idle
+Spotted - Sees the player. Alert for a little bit, then give chase
+Active Chase - Is giving chase and can still see the player. Constantly pathfind to the node closest to the player. If the player is within range, attack the player
+Passive Chase - Is giving chase and cannot see the player. Stop at node closest to last known player location. Downgrade to Alert when node is reached.
+Smells Meat - Player has dropped meat somewhere on the floor. Walk to Meat
+Eating - Reached meat that isn't in a trap. Consume meat for a time.
+Trapped - Has walked into a beartrap. Whimper and try to free seld
+Dead - Shot by player. Dead
+]]
+
 Monster_Wolf = Class {
 	init = function(self)
-	
+
 	end
 };
 
 function Monster_Wolf:update(dt)
-	
+
 end
 
 function Monster_Wolf:draw()
-	
+
 end
