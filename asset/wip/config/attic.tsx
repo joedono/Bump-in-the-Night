ef11c067b1bacb0624be120fc36b905357b9f08f
joedono <zidane320@yahoo.com>
2018-01-02 11:31:38 -0500

    Refactored tileset images and added decorations tileset

--- asset/wip/config/attic.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="Attic" tilewidth="32" tileheight="32" tilecount="117" columns="9">
- <image source="../../image/tiles/tilesetHouse.png" width="288" height="416"/>
-</tileset>
---
+++ asset/wip/config/decorations.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Decorations" tilewidth="32" tileheight="32" tilecount="256" columns="16">
+ <image source="../../image/tiles/interior.png" width="512" height="512"/>
+</tileset>
