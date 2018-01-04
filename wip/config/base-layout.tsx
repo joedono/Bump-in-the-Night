3d07156258eb8f522159d3949c692af6ee2ddfd2
joedono <zidane320@yahoo.com>
2018-01-04 14:05:38 -0500

    Moved wip to new parent-level folder

--- wip/config/base-layout.tsx
+++ wip/config/base-layout.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset name="Main Floor" tilewidth="32" tileheight="32" tilecount="117" columns="9">
- <image source="../../image/tiles/tilesetHouse.png" trans="ffffff" width="288" height="416"/>
+ <image source="../../asset/image/tiles/tilesetHouse.png" trans="ffffff" width="288" height="416"/>
 </tileset>
