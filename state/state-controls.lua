6e649a80a19fa801a5eb74981659fd50f278e76e
joedono <zidane320@yahoo.com>
2018-01-04 17:09:08 -0500

    Added controls screen

--- state/state-controls.lua
+++ state/state-controls.lua
@@ -1,6 +1,9 @@
 State_Controls = {};
 
 function State_Controls:init()
+	self.image = love.graphics.newImage("asset/image/screen/controls.png");
+	self.titleFont = love.graphics.newFont("asset/font/Fiendish.ttf", 30);
+	self.controlsFont = love.graphics.newFont("asset/font/Fiendish.ttf", 16);
 end
 
 function State_Controls:enter()
@@ -21,8 +24,22 @@ end
 function State_Controls:draw()
 	CANVAS:renderTo(function()
     love.graphics.clear();
-		love.graphics.setColor(255, 255, 255, 255);
-		love.graphics.print("Controls", 0, 0);
+		love.graphics.setColor(255, 0, 0);
+		love.graphics.draw(self.image, 0, 0);
+		love.graphics.setFont(self.titleFont);
+		love.graphics.printf("Controls", 0, 12, SCREEN_WIDTH, "center");
+
+		love.graphics.setFont(self.controlsFont);
+		love.graphics.print("L1/LB - Previous Item", 50, 195);
+		love.graphics.print("Select/Back - Quit", 630, 125);
+		love.graphics.print("Start - Pause", 800, 175);
+		love.graphics.print("R1/RB - Next Item", 1315, 195);
+		love.graphics.print("R2/RT - Run", 1315, 105);
+		love.graphics.print("DPad/L Joystick - Walk", 210, 725);
+		love.graphics.print("R Joystick - Flashlight", 1040, 725);
+		love.graphics.print("Use Item", 1275, 455);
+
+		love.graphics.printf("Press any button to go back", 0, 840, SCREEN_WIDTH, "center");
   end);
 
   love.graphics.setColor(255, 255, 255);
