24cb557a404f755bd725451d3e86bcd17c7ab3c0
joedono <zidane320@yahoo.com>
2018-01-29 17:30:57 -0500

    Added notes to vampire

--- monster/monster-vampire.lua
+++ monster/monster-vampire.lua
@@ -1,13 +1,24 @@
+--[[
+Moves randomly and hunts by sight.
+Cannot be seen unless within range of his ability.
+Moving at all while within range of his ability will freeze the player and cause the vampire to move slowly towards the player.
+Attempting to move will gradually break the spell.
+Holding the cross will make the player immune to the freeze and the immediate-detect effect,
+allowing them to get close enough to use the stake.
+
+
+]]
+
 Monster_Vampire = Class {
 	init = function(self)
-	
+
 	end
 };
 
 function Monster_Vampire:update(dt)
-	
+
 end
 
 function Monster_Vampire:draw()
-	
+
 end
