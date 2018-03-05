0603b632d0d51d6d4de3fea9ae4053a4ca786260
joedono <zidane320@yahoo.com>
2018-03-04 21:14:46 -0500

    Added carpetting

---
+++ wip/config/carpet.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Carpet" tilewidth="32" tileheight="32" tilecount="84" columns="12">
+ <image source="../../asset/image/tiles/carpet.png" width="384" height="224"/>
+</tileset>
