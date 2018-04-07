81d8bde967f92dbc0a16b5b2566ff685fb460ee0
joedono <zidane320@yahoo.com>
2018-04-06 22:06:25 -0400

    Updated build script

--- BUILDME.ps1
+++ BUILDME.ps1
@@ -1,19 +1,20 @@
+$Project = 'Bump-in-the-Night'
 $Version = Read-Host -Prompt 'Version Number?'
 $path = cd
 $exclude = @("notes", "wip", "BUILDME.ps1", "README.md", "RUNME.bat")
 $files = Get-ChildItem -Path $path -Exclude $exclude
 
-Compress-Archive -Path $files -DestinationPath "Bump-in-the-Night_${Version}.zip"
-Rename-Item -Path "Bump-in-the-Night_${Version}.zip" -NewName "Bump-in-the-Night_${Version}.love"
+Compress-Archive -Path $files -DestinationPath "${Project}_${Version}.zip"
+Rename-Item -Path "${Project}_${Version}.zip" -NewName "${Project}_${Version}.love"
 
-cmd.exe /c copy /b "E:\Program Files\LOVE\love.exe"+"Bump-in-the-Night_$Version.love" "Bump-in-the-Night.exe"
-Rename-Item -Path "Bump-in-the-Night.exe" -NewName "Bump-in-the-Night_${Version}_Win_x64.exe"
-Compress-Archive -Path "E:\Program Files\LOVE\*.dll", "Bump-in-the-Night_${Version}_Win_x64.exe" -DestinationPath "Bump-in-the-Night_${Version}_Win_x64.zip"
-Remove-Item -Path "Bump-in-the-Night_${Version}_Win_x64.exe"
+cmd.exe /c copy /b "E:\Program Files\LOVE\love.exe"+"${Project}_$Version.love" "${Project}.exe"
+Rename-Item -Path "${Project}.exe" -NewName "${Project}_${Version}_Win_x64.exe"
+Compress-Archive -Path "E:\Program Files\LOVE\*.dll", "${Project}_${Version}_Win_x64.exe" -DestinationPath "${Project}_${Version}_Win_x64.zip"
+Remove-Item -Path "${Project}_${Version}_Win_x64.exe"
 
-cmd.exe /c copy /b "E:\Program Files (x86)\LOVE\love.exe"+"Bump-in-the-Night_$Version.love" "Bump-in-the-Night.exe"
-Rename-Item -Path "Bump-in-the-Night.exe" -NewName "Bump-in-the-Night_${Version}_Win_x86.exe"
-Compress-Archive -Path "E:\Program Files (x86)\LOVE\*.dll", "Bump-in-the-Night_${Version}_Win_x86.exe" -DestinationPath "Bump-in-the-Night_${Version}_Win_x86.zip"
-Remove-Item -Path "Bump-in-the-Night_${Version}_Win_x86.exe"
+cmd.exe /c copy /b "E:\Program Files (x86)\LOVE\love.exe"+"${Project}_$Version.love" "${Project}.exe"
+Rename-Item -Path "${Project}.exe" -NewName "${Project}_${Version}_Win_x86.exe"
+Compress-Archive -Path "E:\Program Files (x86)\LOVE\*.dll", "${Project}_${Version}_Win_x86.exe" -DestinationPath "${Project}_${Version}_Win_x86.zip"
+Remove-Item -Path "${Project}_${Version}_Win_x86.exe"
 
 pause
