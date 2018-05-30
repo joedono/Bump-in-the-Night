38f4213e7d0bb882f38b0effa6c4b64fadfa6d49
joedono <fjzusi@gmail.com>
2018-05-30 16:17:37 -0400

    Removed unused variable

--- house/manager-ray-guns.lua
+++ house/manager-ray-guns.lua
@@ -6,30 +6,25 @@ Manager_Ray_Guns = Class {
 		self.soundEffects = soundEffects;
 
 		self.rays = {};
-		self.numRays = 0;
 		self.rayExplodeImage = love.graphics.newImage("asset/image/noise.png");
 	end
 };
 
 function Manager_Ray_Guns:shootRayGun(x, y, targetX, targetY)
   table.insert(self.rays, Ray(self, x, y, targetX, targetY, self.rayExplodeImage));
-  self.numRays = self.numRays + 1;
 end
 
 function Manager_Ray_Guns:update(dt)
 	local activeRays = {};
-	local numRays = 0;
 
 	for index, ray in pairs(self.rays) do
 		if ray.active then
       ray:update(dt);
 			table.insert(activeRays, ray);
-			numRays = numRays + 1;
 		end
 	end
 
 	self.rays = activeRays;
-	self.numRays = numRays;
 end
 
 function Manager_Ray_Guns:draw()
